e684e5077a5aa4b1f8c4b6bb2d6863e5a16f6649
Vishwakant-coder <work.vishwakant.edu@gmail.com>
2024-08-08 23:23:47 +0530

    day one

---
+++ 08_August_2024_Reverse_sub_array/Reverse _sub_array.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    vector<int> reverseSubArray(vector<int> &arr, int l, int r)
+    {
+        // code here
+        while (l < r)
+        {
+            int temp = arr[l - 1];
+            arr[l - 1] = arr[r - 1];
+            arr[r - 1] = temp;
+            l++;
+            r--;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--)
+    {
+        string ls;
+        getline(cin, ls);
+        int l = stoi(ls);
+        string rs;
+        getline(cin, rs);
+        int r = stoi(rs);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.reverseSubArray(arr, l, r);
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 100-Day-Coding-Challenge
+
 Start from 08-08-2024
+Day one(08-08-2-24), question:- Reverse sub array
