5c5d8f660ea36ff8ad33e70b10611fb635ac0e9d
Vishwakant-coder <work.vishwakant.edu@gmail.com>
2024-10-22 00:02:11 +0530

    Day Seventy-four

---
+++ 20-10-2024/Split the Array.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int countgroup(vector<int>& arr) {
+        // Complete the function
+        int n =arr.size();
+        int val =0;
+        for(int i=1;i<n;i++){
+            val^=arr[i];
+        }
+        if(val==arr[0]){
+            return n;
+        }
+        return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int ans = obj.countgroup(arr);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
--- README.md
+++ README.md
@@ -74,3 +74,4 @@ Day Seventy (16-10-2024), question:- Split Linked List Alternatingly <br>
 Day Seventy-one (17-10-2024), question:- Single Number <br>
 Day Seventy-two (18-10-2024), question:- SNearest multiple of 10 <br>
 Day Seventy-three (19-10-2024), question:- Sort a k sorted doubly linked list <br>
+Day Seventy-three (20-10-2024), question:- Split the Array <br>
